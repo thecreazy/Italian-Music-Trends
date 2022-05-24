c73af191c914d246a41cb19d2131dd73a1207441
thecreazy <canella_riccardo@icloud.com>
2022-05-24 13:15:09 +0200

    feat(core): add numberOfWords informations

--- src/lyricsToWords/main.js
+++ src/lyricsToWords/main.js
@@ -23,7 +23,7 @@ const main = () => {
     const noSpaStopWords = stopword.removeStopwords(noItaStopWords, stopword.spa);
     const uniqueValues = noSpaStopWords.filter(onlyUnique);
     const noEmptyValues = uniqueValues.filter((v) => !!v);
-    lyrics[trackInfo.id] = noEmptyValues;
+    lyrics[trackInfo.id] = {values: noEmptyValues, numberOfWords: noEmptyValues.length}
   }
   fs.writeFileSync(path.resolve(__dirname, '../../files/calculated/wordsForLyrics.json'), JSON.stringify(lyrics, null, 4));
 };
